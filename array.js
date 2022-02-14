d1e6c7dd35dc66b2553440f89625168eb45526dd
Varshini24V <varshinibaby24@example.com>
2022-02-14 14:35:12 +0530

    Commit

---
+++ array.js
@@ -0,0 +1,10 @@
+const ps= require("prompt-sync");
+const prompt=ps();
+var inputArray = [];
+n=prompt("Enter the length ");
+console.log("Enter the Elements");
+for(var i=0; i<n; i++) 
+{
+	inputArray[i] = prompt( );
+}
+console.log(inputArray);
